df76bea63424ff560b9691adc1cfd19eafe556bb
OmPrakash <omprakash.sahoo@hotmail.com>
2019-07-21 15:27:58 +0530

    ANN_churning_rate_bank_customer

--- Keras_bank_churning_custpmer_ANN/Churning_rate_of_bank_customer_ANN.py
+++ Keras_bank_churning_custpmer_ANN/Churning_rate_of_bank_customer_ANN.py
@@ -22,7 +22,7 @@ X[:,2] = le_x2.fit_transform(X[:,2])
 onehotencoder = OneHotEncoder(categorical_features = [1])
 X= onehotencoder.fit_transform(X).toarray()
 #dummy variable trap to reduce remove 1 variable
-X= X[:,1:]
+X= X[:,1:]  #France(0,0),spain(0,1),germany(1,0)
 
 #train_test split
 from sklearn.model_selection import train_test_split
@@ -39,6 +39,7 @@ import keras
 #model used to initialise the NN
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Dropout   # to decrease overfitting
 se = Sequential()
 
 #add input layer and output layer  11+1 /2 = 6 hidden layer 
@@ -46,7 +47,7 @@ se = Sequential()
 # uniform function initialise the weight randomly
 se.add(Dense(output_dim =6,activation = 'relu',kernel_initializer = 'uniform', 
              input_dim = 11))
-
+se.add(Dropout(rate = 0.1))
 # add 2nd hidden layer 
 se.add(Dense(6,activation = 'relu',kernel_initializer = 'uniform'))
 
@@ -70,10 +71,79 @@ cm = confusion_matrix(Y_test,Y_pred)
 print(cm)
 
 
+# Predicting a single new observation
+"""Predict if the customer with the following informations will leave the bank:
+Geography: Spain
+Credit Score: 700
+Gender: Female
+Age: 45
+Tenure: 4
+Balance: 70000
+Number of Products: 3
+Has Credit Card: Yes
+Is Active Member: Yes
+Estimated Salary: 55000"""
+#insert into ARRAY in same order HORIZONTALLY,so add 2d array [[]] 
+#Apply the same scaller(sc) for stanardised the data
+pred_customer= se.predict(sc.transform(np.array([[0.0, 1, 700, 0, 45, 4, 70000, 3, 1, 1, 55000]])))
+pred_customer = (pred_customer > 0.5)
+
+#---------------------------------------------------------------------------
 
 
+# Evaluating the ANN
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from keras.models import Sequential
+from keras.layers import Dense
+#from keras.layers import Dropout
+def build_classifier():
+    se = Sequential()
+    se.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+    #se.add(Dropout(rate = 0.1)) 
+    se.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    se.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    se.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return se
 
+#se is local variable make 1st global varible to use outside it
+se = KerasClassifier(build_fn = build_classifier, batch_size = 10, epochs = 100)
+#cross-validation -cv -1 is all cpu we are using parallel processing
+accuracies = cross_val_score(estimator=se,X = X_train,y=Y_train,cv=10,n_jobs=-1)
+mean = accuracies.mean()
+variance = accuracies.std()   #high variance = overfitting
 
+#--------------------------------------------------------------------------
+
+#parameter tuning find the best value of hyper parameter(best choice make)
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+#we pass parameter in function to choose the optimiser parameter 
+def build_classifier(optimizer):
+    se = Sequential()
+    se.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+    se.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    se.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    #instead od adam we will choose between adam and rmsprop. 
+    se.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return se
+
+se = KerasClassifier(build_fn = build_classifier)
+#define the hyper parameter which you want to tune for best fit model
+parameters = {'batch_size': [25, 32],
+              'epochs': [100, 500],
+              'optimizer': ['adam', 'rmsprop']}
+
+grid_search = GridSearchCV(estimator = se,
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 10)
+#fit grid search to training set
+grid_search_new = grid_search.fit(X_train, Y_train)
+best_parameters = grid_search_new.best_params_
+best_accuracy = grid_search_new.best_score_
 
 
 
